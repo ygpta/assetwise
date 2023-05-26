034fad6a5c8c8357d10b881380629f06a40877be
ygpta <yatendraguptaofficial@gmail.com>
2023-05-26 18:50:57 +0530

    querying the user data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,35 +2,31 @@ import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 // import { useEffect } from "react";
-import Sidebar from "~/components/Sidebar";
 import Hero from "~/components/Hero";
+import Sidebar from "~/components/Sidebar";
 import { api } from "~/utils/api";
 // import { useRouter } from "next/router";
 
-
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
-console.log("sessionData",sessionData)
-const data=api.user.getUserDetails.useQuery()
+  console.log("sessionData", sessionData);
+  const { data } = api.user.getUserDetails.useQuery();
 
-console.log("data",data)
-  
-    return (
-      <>
-        <Head>
-          <title>Assetwise</title>
-          <meta name="description" content="Assetwise" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className="flex flex-row justify-start bg-white align-middle">
-          <Sidebar />
-          <Hero />
-        </main>
-      </>
-    );
+  console.log("data", data);
+
+  return (
+    <>
+      <Head>
+        <title>Assetwise</title>
+        <meta name="description" content="Assetwise" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-row justify-start bg-white align-middle">
+        <Sidebar />
+        <Hero />
+      </main>
+    </>
+  );
 };
 
 export default Home;
-
-
-
