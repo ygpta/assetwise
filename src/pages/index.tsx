a607a8d30cccea4653fe50a6cc570f3ea6bc869c
ygpta <yatendraguptaofficial@gmail.com>
2023-05-26 18:42:20 +0530

    api to get all the current user data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,20 @@
 import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Sidebar from "~/components/Sidebar";
 import Hero from "~/components/Hero";
-import { useRouter } from "next/router";
+import { api } from "~/utils/api";
+// import { useRouter } from "next/router";
 
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
-const router = useRouter();
-
 console.log("sessionData",sessionData)
-useEffect(() => {
-  if (!sessionData) {
-    router.push(
-      "/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F"
-    );
-  }
-}, [sessionData])
+const data=api.user.getUserDetails.useQuery()
 
-
+console.log("data",data)
+  
     return (
       <>
         <Head>
---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,14 @@
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  getUserDetails: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id,
+      },
+      include: {
+        asset: true,
+      },
+    });
+  }),
+});
